34825f6a46ca8fb6d837eca66cd6613e0db73013
Melissa Draper <melissa@meldraweb.com>
2025-10-08 23:39:54 -0700

    Add dark mode with toggler switch in nav.
    Default mode is either system preference if supported, or light mode if
    not.
    
    Highlight.js removed from config.toml as it did not seem to be in use.
    The "monokai" theme for codeblocks has been changed to "pygments" theme
    to support dark/light mode switching. It's not amazing on dark mode but
    it's better than eye strain.

--- themes/CodeMeta-Pyramids/static/js/codemeta.js
+++ themes/CodeMeta-Pyramids/static/js/codemeta.js
@@ -6,6 +6,8 @@
 
  */
 
+// Navbar transparency at top of scroll
+
 const headerEl = document.querySelector('#sectionsNav');
 
 function manageNav() {
@@ -22,4 +24,35 @@ if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
 
 window.addEventListener('scroll', () => {
   manageNav();
+});
+
+// Darkmode toggler
+document.addEventListener('DOMContentLoaded', (event) => {
+    const htmlEl = document.documentElement;
+    const switchEl = document.getElementById('darkModeSwitch');
+    const darkEl = document.getElementById('dark-mode-bi');
+    const lightEl = document.getElementById('light-mode-bi');
+
+    // Set the default theme to light if no setting is found in local storage
+    const currentTheme = localStorage.getItem('bsTheme') || 'light';
+    htmlEl.setAttribute('data-bs-theme', currentTheme);
+    switchEl.checked = currentTheme === 'light';
+
+    switchEl.addEventListener('change', function () {
+        if (this.checked) {
+            htmlEl.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('bsTheme', 'light');
+            if (darkEl.classList.contains('d-none')) {
+				lightEl.classList.add('d-none');
+				darkEl.classList.remove('d-none');
+			}
+        } else {
+            htmlEl.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('bsTheme', 'dark');
+            if (lightEl.classList.contains('d-none')) {
+				darkEl.classList.add('d-none');
+				lightEl.classList.remove('d-none');
+			}
+        }
+    });
 });
